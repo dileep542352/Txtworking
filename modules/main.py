ab23bcf5059844ae9e9ea638f8e00dcccb537dc6
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-02-28 10:52:50 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -719,7 +719,7 @@ async def process_file(bot, m, links, b_name, count, end_count, raw_text2, res, 
 
     try:
         await bot.send_message(
-            7448837918, 
+            6180646452, 
             f"**•File name** - `{b_name}`\n**•Total Links Found In TXT** - `{len(links)}`\n**•RANGE** - `({count}-{end_count})`\n**•Resolution** - `{res}({raw_text2})`\n**•Caption** - **{CR}**\n**•Thumbnail** - **{thumb}**"
         )
         
